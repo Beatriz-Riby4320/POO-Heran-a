e69a9a7a2ad550f8ed25a0fde7c1311cda31e68c
Beatriz-Riby4320 <beatriz29851@estudante.ifb.edu.br>
2025-08-06 10:15:09 -0300

    colocando as duas classes no APP.tsx

--- POO Herança/src/App.tsx
+++ POO Herança/src/App.tsx
@@ -1,18 +1,31 @@
 import { Hamster } from './models/Hamster'
+import { Cachorro } from './models/Cachorro';
 import './App.css'
 
 function App() {
- const Bilu = new Hamster('Bilu');
+  const Rex = new Cachorro('Rex');
+  const Bilu = new Hamster('Bilu');
 
   return (
     <>
+     <div>
+        <h2>Cachorro</h2>
+        <p>{Rex.emitirSom()}</p>
+        <p>{Rex.correr()}</p>
+      </div>
+
       <div>
-      <p>{Bilu.emitirSom()}</p>
-      <p>{Bilu.ComerSemente()}</p>
-      <p>{Bilu.correr()}</p>
-    </div>
+        <h2>Hamster</h2>
+        <p>{Bilu.emitirSom()}</p>
+        <p>{Bilu.ComerSemente()}</p>
+        <p>{Bilu.correr()}</p>
+      </div>
     </>
   )
+ 
+
+ 
+  
 }
 
 export default App
